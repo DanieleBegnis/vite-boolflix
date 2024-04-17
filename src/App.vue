a2005617d7438087a096821a915774bb0559209b
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-17 16:06:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+
+}
+</script>
+
+<template>
+<div>test test</div>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
+
