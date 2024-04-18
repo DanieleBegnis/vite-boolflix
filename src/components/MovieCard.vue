312f3572c12b7ae669404e732ef3f53b95bae385
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-18 19:49:47 +0200

    feat: add stars

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,17 +1,44 @@
 <script>
+// import { store } from './store.js';
 
 export default {
     name: 'MovieCard',
     props: {
         cardInfo: Object
     },
+    data() {
+        return {
+
+        };
+    },
     methods: {
         roundVote() {
-            console.log(roundVote)
-            Math.ceil(cardInfo.vote_average)
-            
+            const vote = this.cardInfo.vote_average
+            let star = Math.ceil(vote / 2)
+            // for (let i = 0; i <= star; i++) {
+            //     store.stars.push(n)
+            // }
+            return star
+            // switch (star) {
+            //     case 1:
+            //     `${<i class="fa-solid fa-star"></i>}`;
+            //         break;
+            //     case 2:
+            //     console.log('2 stella');
+            //         break;
+            //     case 3:
+            //     console.log('3 stella');
+            //         break;
+            //     case 4:
+            //     console.log('4 stella');
+            //         break;
+            //     case 5:
+            //     `${<i class="fa-solid fa-star"></i>}`;
+            //         break;
+            // }
         }
     }
+
 }
 </script>
 
@@ -44,38 +71,50 @@ export default {
                 </div>
                 <div class="language" v-else>{{ cardInfo.original_language }}</div>
             </div>
-            <!-- <div class="review">{{ roundVote() }}</div> -->
+            <div class="review" v-if="roundVote(star) === 4">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+
+            </div>
             <div class="overwiew">{{ cardInfo.overview }}</div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-    .single-card {
-        width: calc((100% / 3) - 30px);
-        text-align: center;
-        margin: 15px;
-        background-color: #000;
-        border: 3px solid lightgray;
-        div {
-            color: #fff;
-            margin: 12px 0;
-        }
-        p {
-            margin-bottom: 6px;
-        }
-    }
-    .language-img {
-        width: 16px;
-    }
-    .card-informations {
-        display: none;
-    }
-    .single-card:hover .card-informations {
-        display: block;
-    }
-    .single-card:hover .card-image {
-        display: none;
+.single-card {
+    width: calc((100% / 3) - 30px);
+    text-align: center;
+    margin: 15px;
+    background-color: #000;
+    border: 3px solid lightgray;
+
+    div {
+        color: #fff;
+        margin: 12px 0;
     }
 
+    p {
+        margin-bottom: 6px;
+    }
+}
+
+.language-img {
+    width: 16px;
+}
+
+.card-informations {
+    display: none;
+}
+
+.single-card:hover .card-informations {
+    display: block;
+}
+
+.single-card:hover .card-image {
+    display: none;
+}
 </style>
